e0b096f6a91d3280192c2286cf92e2f7f0374320
GustavoSF643 <gustavosilvafranco643@gmail.com>
2026-05-28 11:32:40 -0300

    test: align e2e setup with path aliases

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,7 +4,7 @@ import request from "supertest";
 import { App } from "supertest/types";
 import { AppModule } from "./../src/app.module";
 
-describe("AppController (e2e)", () => {
+describe("App (e2e)", () => {
   let app: INestApplication<App>;
 
   beforeAll(async () => {
@@ -20,10 +20,7 @@ describe("AppController (e2e)", () => {
     await app.close();
   });
 
-  it("/ (GET)", () => {
-    return request(app.getHttpServer())
-      .get("/")
-      .expect(200)
-      .expect("Hello World!");
+  it("GET /users without token returns 401", () => {
+    return request(app.getHttpServer()).get("/users").expect(401);
   });
 });
